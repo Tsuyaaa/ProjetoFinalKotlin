6856943bf98cb0bc4fa8c9a203b166e750c9de53
Tsuyaaa <imtsuya@gmail.com>
2022-03-14 12:45:39 -0300

    Formatação de texto

--- src/main/kotlin/Cachorro.kt
+++ src/main/kotlin/Cachorro.kt
@@ -1,17 +1,21 @@
-class Cachorro(nome: String,
-               idade: Int,
-               peso: String,
-               sexo: String,
-               private val raca: String):
+class Cachorro(
+    nome: String,
+    idade: Int,
+    peso: String,
+    sexo: String,
+    private val raca: String
+) :
     Animal(nome, idade, peso, sexo) {
 
-    val listaCachorro = mutableListOf<String>("V8: previnem: cinomose, parvovirose, hepatite infecciosa canina," +
-            " adenovirose tipo II, parainfluenza, coronavirose, leptospirose.",
+    val listaCachorro = mutableListOf<String>(
+        "V8: previnem: cinomose, parvovirose, hepatite infecciosa canina," +
+                " adenovirose tipo II, parainfluenza, coronavirose, leptospirose.",
         "V10: contempla proteção contra mais dois tipos de cepas causadoras da leptospirose",
         "Gripe: é opcional, mas é recomendada para cachorros que convivem com outros cães," +
                 " que frequentam petshops, hospedagens, parques e praças",
         "Giárdia: é opcional e recomendada se o cachorro convive com outros pets, passeia" +
-                " ou se vive em um local com saneamento básico precário. ")
+                " ou se vive em um local com saneamento básico precário. "
+    )
 
     fun age() {
         while (true) {
@@ -27,52 +31,58 @@ class Cachorro(nome: String,
     }
 
     override fun obrig() {
-        if (idade in 0..1){
+        if (idade <1) {
             //println("Vacinação obrigatória: ")
             println("Como $nome tem menos de 1 ano. \nDigite a idade em meses: ")
 
-            while(true){
-                try{
+            while (true) {
+                try {
                     idade = readLine()!!.toInt()
                     break
 
-                }catch(e: Exception){
+                } catch (e: Exception) {
                     println("Valor inválido.")
 
                 }
             }
-            if(idade in 2..3){
-                println("$nome pode tomar a primeira dose:\n")
-                listaCachorro.forEach{
+            if (idade in 2..3) {
+                println("$nome pode tomar a primeira dose das vacinas:\n")
+                listaCachorro.forEach {
                     println(it)
                 }
 
 
-            }else if(idade in 4..12){
+            } else if (idade in 4..12) {
                 println("$nome deve tomar:\n")
-                listaCachorro.add("Antirrábica: obrigatória no território nacional," +
-                        " protege seu pet de uma infecção viral grave e sem tratamento para cachorros.")
+                listaCachorro.add(
+                    "Antirrábica: obrigatória no território nacional," +
+                            " protege seu pet de uma infecção viral grave e sem tratamento para cachorros."
+                )
 
-                listaCachorro.forEach{
+                listaCachorro.forEach {
                     println(it)
                 }
 
 
-            }else{
-                println("Seu bichinho esta com déficit de vacinação, necessário:\n")
-                listaCachorro.add("Antirrábica: obrigatória no território nacional," +
-                        " protege seu pet de uma infecção viral grave e sem tratamento para cachorros.")
+            } else {
+                println("Se o seu bichinho ainda não se vacinou, ele está com déficit de vacinação, será necessário:\n")
+                listaCachorro.add(
+                    "Antirrábica: obrigatória no território nacional," +
+                            " protege seu pet de uma infecção viral grave e sem tratamento para cachorros."
+                )
 
-                listaCachorro.forEach{
+                listaCachorro.forEach {
                     println(it)
                 }
             }
-        }else{
-            println("Seu bichinho esta com déficit de vacinação, necessário:\n")
-            listaCachorro.add("Antirrábica: obrigatória no território nacional," +
-                    " protege seu pet de uma infecção viral grave e sem tratamento para cachorros.")
+        } else {
+            println("Se o seu bichinho ainda não se vacinou, ele está com déficit de vacinação, será necessário:\n")
+            listaCachorro.add(
+                "Antirrábica: obrigatória no território nacional," +
+                        " protege seu pet de uma infecção viral grave e sem tratamento para cachorros."
+            )
 
-            listaCachorro.forEach{
+            listaCachorro.forEach {
                 println(it)
             }
         }
--- src/main/kotlin/Gato.kt
+++ src/main/kotlin/Gato.kt
@@ -1,13 +1,17 @@
-class Gato (nome: String,
-                     idade: Int,
-                     peso: String,
-                     sexo: String,
-                     private val raca: String):
-            Animal(nome, idade, peso, sexo){
+class Gato(
+    nome: String,
+    idade: Int,
+    peso: String,
+    sexo: String,
+    private val raca: String
+) :
+    Animal(nome, idade, peso, sexo) {
 
-    val listaGato = mutableListOf<String>("V3: previne rinotraqueíte, calicivirose e panleucopenia",
+    val listaGato = mutableListOf<String>(
+        "V3: previne rinotraqueíte, calicivirose e panleucopenia",
         "V4: previne as doenças contempladas pela V3 mais a Clamidiose",
-        "V5: previne as doenças contempladas pela V4 mais a Leucemia viral felina")
+        "V5: previne as doenças contempladas pela V4 mais a Leucemia viral felina"
+    )
 
     fun age() {
         while (true) {
@@ -23,55 +27,61 @@ class Gato (nome: String,
     }
 
     override fun obrig() {
-        if (idade in 0..1){
+        if (idade < 1) {
             //println("Vacinação obrigatória: ")
             println("Como $nome tem menos de 1 ano. \nDigite a idade em meses: ")
 
-            while(true){
-                try{
+            while (true) {
+                try {
                     idade = readLine()!!.toInt()
                     break
 
-                }catch(e: Exception){
+                } catch (e: Exception) {
                     println("Valor inválido.")
 
                 }
-           }
-            if(idade in 2..3){
-                println("$nome pode tomar a primeira dose:\n")
-                listaGato.forEach{
+            }
+            if (idade in 2..3) {
+                println("$nome pode tomar a primeira dose das vacinas:\n")
+                listaGato.forEach {
                     println(it)
                 }
 
 
-            }else if(idade in 4..12){
+            } else if (idade in 4..12) {
                 println("$nome deve tomar:\n")
-                listaGato.add("Antirrábica: essa vacina é obrigatória no território nacional. " +
-                        "A vacina contra a raiva protege seu pet de uma infecção viral grave " +
-                        "e sem tratamento para gatos.")
+                listaGato.add(
+                    "Antirrábica: essa vacina é obrigatória no território nacional. " +
+                            "A vacina contra a raiva protege seu pet de uma infecção viral grave " +
+                            "e sem tratamento para gatos."
+                )
 
-                listaGato.forEach{
+                listaGato.forEach {
                     println(it)
                 }
 
 
-            }else{
-                println("Seu bichinho esta com déficit de vacinação, necessário:\n")
-                listaGato.add("Antirrábica: essa vacina é obrigatória no território nacional. " +
-                        "A vacina contra a raiva protege seu pet de uma infecção viral grave " +
-                        "e sem tratamento para gatos.")
+            } else {
+                println("Se o seu bichinho ainda não se vacinou, ele está com déficit de vacinação, será necessário:\n")
+                listaGato.add(
+                    "Antirrábica: essa vacina é obrigatória no território nacional. " +
+                            "A vacina contra a raiva protege seu pet de uma infecção viral grave " +
+                            "e sem tratamento para gatos."
+                )
 
-                listaGato.forEach{
+                listaGato.forEach {
                     println(it)
                 }
             }
-        }else{
-            println("Seu bichinho esta com déficit de vacinação, necessário:\n")
-            listaGato.add("Antirrábica: essa vacina é obrigatória no território nacional. " +
-                    "A vacina contra a raiva protege seu pet de uma infecção viral grave " +
-                    "e sem tratamento para gatos.")
+        } else {
+            println("Se o seu bichinho ainda não se vacinou, ele está com déficit de vacinação, será necessário:\n")
+            listaGato.add(
+                "Antirrábica: essa vacina é obrigatória no território nacional. " +
+                        "A vacina contra a raiva protege seu pet de uma infecção viral grave " +
+                        "e sem tratamento para gatos."
+            )
 
-            listaGato.forEach{
+            listaGato.forEach {
                 println(it)
             }
         }
@@ -85,7 +95,6 @@ class Gato (nome: String,
                 "\nIdade - $idade" +
                 "\nPeso - $peso kg"
     }
-
      */
 
 }
