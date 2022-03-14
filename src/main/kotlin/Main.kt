776d466eaf2f01789e9729aeaef75c58e4dbbb42
Tsuyaaa <imtsuya@gmail.com>
2022-03-14 12:47:12 -0300

    Texto atualizado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,54 @@
 fun main() {
-    println("Cartela de vacinação obrigatória: ")
-    print("Você possui gato ou cachorro?\nR.: ")
+
+    println("Cartela de vacinação obrigatória\n")
+    print("Olá! O seu pet é um gato ou um cachorro?\nR: ")
     val animalEscolhido = readLine()!!.lowercase()
 
-    println("O seu animal é $animalEscolhido")
-    print("\nDigite o nome: ")
+    println("Ok! O seu pet é $animalEscolhido")
+    print("Por favor, digite o nome do seu pet:\nR: ")
     val nome = readLine()!!
 
     /*
     print("Digite a idade: ")
     var idade = readLine()!!.toInt()
-
      */
 
-    print("Digite o peso: ")
+    print(
+        "Por favor, digite o peso:\n" +
+
+                "R: "
+    )
     val peso = readLine()!!
 
-    print("Digite o sexo: ")
+    print(
+        "Certo, qual o sexo do animal?\n" +
+
+                "R: "
+    )
     val sexo = readLine()!!
 
-    print("Digite a raça: ")
+    print(
+        "Okay! Estamos quase terminando. Por favor, digite a raça:\n" +
+
+                "R: "
+    )
     val raca = readLine()!!
 
-    if(animalEscolhido == "gato"){
+    if (animalEscolhido == "gato") {
         val cat = Gato(nome, 0, peso, sexo, raca)
+
         cat.age()
+
         cat.obrig()
 
-    }else if(animalEscolhido == "cachorro" || animalEscolhido == "cão"){
+
+    } else if (animalEscolhido == "cachorro" || animalEscolhido == "cão") {
         val dog = Cachorro(nome, 0, peso, sexo, raca)
+
         dog.age()
+
         dog.obrig()
     }
+
 
 }
